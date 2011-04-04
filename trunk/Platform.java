ee8efab9f98ff9424734a2ef3665a320a158b1c2
johannes.linke@gmx.de <johannes.linke@gmx.de@01f9bb20-896c-70ea-35b8-c603b6648616>
2011-04-04 21:50:39 +0000

    Kommentare ueberall und Debug.clear und ich weiss nicht mehr

--- trunk/Platform.java
+++ trunk/Platform.java
@@ -1,34 +1,35 @@
 
 public class Platform extends GameObject{
-    
+    /** Der Typ der Plattform. **/
     private final int type;
+    /** Das Item der Plattform, null wenn es kein Item besitzt **/
     public Item item = null;
     
     public static final int NORMAL = 0;
     public static final int BREAK = 1;
     public static final int FAKE = 2;
     
-    /*
-     * Konstrukter fuer Platformen aller Art
-     *
+    /**
+     * Konstruktor fuer Platformen aller Art
      * @param type = Typ der Platform
-     * @param coordinates = koordinaten der Platform
+     * @param x  x-Koordinate der Platform
+     * @param y  y-Koordinate der Platform
      */
     public Platform(int type, double x, double y) {
         super(Tools.platImages[type], x, y);
         this.type = type;
     }
-    /*
-     * Get-Methode fuer den typ
-     *
+    
+    /**
+     * Returned den Typ.
      * @return = Typ der Platform
      */
-    
     public int getType() {
         return type;
     }
-    /*
-     * Methode bewegt die Platform nach unten
+    
+    /**
+     * Bewegt die Platform um die angegebene Distanz nach unten.
      * @param dist = zu bewegende Distanz in pixeln
      */
     public void moveDown(double dist) {
