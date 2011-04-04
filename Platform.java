138d90995335297d06bbb5fc55dae22ef7da8ee1
johannes.linke@gmx.de <johannes.linke@gmx.de@01f9bb20-896c-70ea-35b8-c603b6648616>
2011-04-04 19:50:16 +0000

    ansaetze fuer bewegende plattformen entfernt

--- Platform.java
+++ Platform.java
@@ -8,31 +8,14 @@ public class Platform extends GameObject{
     public static final int BREAK = 1;
     public static final int FAKE = 2;
     
-    private final boolean moves;
-    
-    private final double [][] coords;
-    private int targetCoord = 0;
-    private final double SPEED = 10;
-    private double distToTarget = 0;
-    
     /*
      * Konstrukter fuer Platformen aller Art
      *
      * @param type = Typ der Platform
      * @param coordinates = koordinaten der Platform
      */
-    public Platform(int type, double [][] coordinates) {
-        super(Tools.platImages[type], coordinates[0][0], coordinates[0][1]);
-        this.coords = coordinates;
-        if (coordinates.length > 1) {
-            moves = true;
-            targetCoord = 1;
-            double distX = posX - coords[targetCoord][0];
-            double distY = posY - coords[targetCoord][1];
-            distToTarget = Math.sqrt(distX*distX + distY*distY);
-        }
-        else
-            moves = false;
+    public Platform(int type, double x, double y) {
+        super(Tools.platImages[type], x, y);
         this.type = type;
     }
     /*
@@ -53,33 +36,5 @@ public class Platform extends GameObject{
         if (item != null) {
             item.moveDown(dist);
         }
-    }
-
-    /*
-     * Methode bewegt bewegliche Platformen zur seite.
-     * @param time = Zeit, die sich dei Platform bewegen soll?
-     */
-    public void moveSide(double time) {
-        if (!moves)
-            return;
-        
-        double distX = posX - coords[targetCoord][0];
-        double distY = posY - coords[targetCoord][1];
-        posX += distX * (SPEED/distToTarget);
-        posY += distY * (SPEED/distToTarget);
-        
-        distToTarget = Math.sqrt(distX*distX + distY*distY);
-        
-        setRefPixelPosition((int) posX, (int) posY);
-    }
-
-    /*
-     * Methode gibt den Abstand zum Zielpunkt der seitwärtsbewegung an
-     * @param time = ?
-     */
-    private double distToNextCoord() {
-        double distX = posX - coords[targetCoord][0];
-        double distY = posY - coords[targetCoord][1];
-        return Math.sqrt(distX*distX + distY*distY);
     }
 }
